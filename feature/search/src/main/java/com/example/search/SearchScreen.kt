0bb29dd6683b2bf5fb9a6f8540612b0284ee399a
thangnguyen <thangnguyen@okxe.vn>
2022-06-08 10:53:23 +0700

    add search function

--- feature/search/src/main/java/com/example/search/SearchScreen.kt
+++ feature/search/src/main/java/com/example/search/SearchScreen.kt
@@ -19,7 +19,6 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.nnt.domain.base.Result
 import com.nnt.domain.model.MovieModels
-import kotlinx.coroutines.flow.StateFlow
 import androidx.compose.material.TextFieldDefaults.textFieldColors
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
@@ -33,15 +32,22 @@ import com.nnt.jetpackcomposewithcleanarch.ui.theme.SearchInputBackgroundColor
 fun SearchScreen(navController: NavController){
     val viewModel = hiltViewModel<SearchViewModel>()
     Column {
-        SearchBar(modifier = Modifier.padding(vertical = 10.dp, horizontal = 16.dp))
-        SearchResultList(state = viewModel.searchMovies, navController = navController)
+        SearchBar(
+            modifier = Modifier.padding(vertical = 10.dp, horizontal = 16.dp)
+        ) {
+            viewModel.search(it)
+        }
+        SearchResultList(
+            state = viewModel.searchMovies.collectAsState(Result.Empty),
+            navController = navController
+        )
     }
 }
 
 @ExperimentalUnitApi
 @Composable
-fun SearchResultList(state: StateFlow<Result<MovieModels>>, navController: NavController){
-    when(val result = state.collectAsState().value){
+fun SearchResultList(state: State<Result<MovieModels>>, navController: NavController){
+    when(val result = state.value){
         is Result.Empty -> {
 
         }
@@ -65,7 +71,11 @@ fun SearchResultList(state: StateFlow<Result<MovieModels>>, navController: NavCo
 }
 
 @Composable
-fun SearchBar(modifier: Modifier = Modifier) {
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    searchHint: String = "Search movie",
+    onSearch: (keyword: String) -> Unit
+) {
     var text by rememberSaveable {
         mutableStateOf("")
     }
@@ -73,6 +83,7 @@ fun SearchBar(modifier: Modifier = Modifier) {
         value = text,
         onValueChange = {
             text = it
+            onSearch.invoke(it)
         },
         placeholder = {
             Row {
@@ -81,7 +92,7 @@ fun SearchBar(modifier: Modifier = Modifier) {
                     contentDescription = "search image",
                     colorFilter = ColorFilter.tint(SearchHintColor)
                 )
-                Text(text = "Search movie", color = SearchHintColor)
+                Text(text = searchHint, color = SearchHintColor)
             }
         },
         singleLine = true,
--- feature/search/src/main/java/com/example/search/SearchViewModel.kt
+++ feature/search/src/main/java/com/example/search/SearchViewModel.kt
@@ -6,12 +6,10 @@ import androidx.lifecycle.viewModelScope
 import com.nnt.domain.model.MovieModels
 import com.nnt.domain.usecase.SearchMoviesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 import com.nnt.domain.base.Result
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
@@ -21,15 +19,22 @@ class SearchViewModel @Inject constructor(
     private val _searchMovies = MutableStateFlow<Result<MovieModels>>(Result.Empty)
     val searchMovies: StateFlow<Result<MovieModels>> = _searchMovies
 
-    init {
-        search("spider")
-    }
+    private var searchJob: Job? = null
 
-    fun search(keyword: String?){
-        _searchMovies.value = Result.Loading
-        viewModelScope.launch(Dispatchers.IO){
-            val result = searchMoviesUseCase.execute(keyword = keyword)
-            _searchMovies.value = result
+    fun search(keyword: String?) {
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch{
+            delay(1000)
+            if(keyword.isNullOrEmpty()){
+                _searchMovies.value = Result.Empty
+                return@launch
+            }
+            _searchMovies.value = Result.Loading
+            withContext(Dispatchers.IO){
+                val result = searchMoviesUseCase.execute(keyword = keyword)
+                _searchMovies.value = result
+            }
+
         }
     }
 }
